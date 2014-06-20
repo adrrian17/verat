8de6e0ed3b70cd26575553539679f23e6c70b055
Adrian Ayala <adrian.ayala17@gmail.com>
2014-06-20 15:45:45 -0500

    Adds feature finish command
    This command has an option to delete the feature branch once it’s done

--- lib/verat/feature.rb
+++ lib/verat/feature.rb
@@ -10,5 +10,29 @@ module Verat
 
       puts "Branch created succesfuly. Current branch: feature/#{feature}"
     end
+
+    def self.finish(feature, options)
+      puts "Finishing feature: #{feature}"
+
+      repo   = Git.open(Dir.pwd)
+      branch = "feature/#{feature}"
+
+      if repo.current_branch() != 'develop' and repo.is_branch?(branch)
+        repo.checkout('develop')
+
+        # Temporary fix until git adds options to the merge method
+        system("git merge --no-ff #{branch}")
+      else
+        # Temporary fix until git adds options to the merge method
+        system("git merge --no-ff #{branch}")
+      end
+
+      if options['delete'] then
+        repo.branch("#{branch}").delete
+        puts "Deleting branch: #{branch}"
+      end
+
+      puts "Finished succesfuly. Current branch: develop"
+    end
   end
 end
