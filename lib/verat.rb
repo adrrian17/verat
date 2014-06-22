357791f694ea25e668b76cfcdea4bdf8da9442c3
Adrian Ayala <adrian.ayala17@gmail.com>
2014-06-22 13:48:10 -0500

    Adds hotfix commands

--- lib/verat.rb
+++ lib/verat.rb
@@ -4,4 +4,5 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 module Verat
   autoload :VERSION, 'verat/version'
   autoload :Feature, 'verat/feature'
+  autoload :Hotfix,  'verat/hotfix'
 end
